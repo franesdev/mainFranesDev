4284cadeaf96b68fa4fc6c1bd8ffbe3356158b2f
Franklin Paute <fpaute@itlligen.cloud>
2025-05-06 22:41:29 -0500

    se agrega analisis de pagina

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
+
 
 export const metadata: Metadata = {
   title: 'Franes Dev',
@@ -17,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body>{children}
       <SpeedInsights />
+      <Analytics />
       </body>
     </html>
   )
